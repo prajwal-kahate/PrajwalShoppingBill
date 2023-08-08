fb0dec3f271ae64158e7cc9a34564fba0c43a383
prajwal-kahate <137600937+prajwal-kahate@users.noreply.github.com>
2023-08-09 00:02:15 +0530

    Add files via upload

---
+++ ShoppingBill/Product.java
@@ -0,0 +1,56 @@
+package com.ShoppingBill;
+
+public class Product{
+	
+	private String id;
+	private String pname;
+	private int qty;
+	private double price;
+	private double totalPrice;
+
+	// constructor
+	Product(String id, String pname, int qty, double price, double totalPrice) {
+		this.id = id;
+		this.pname = pname;
+		this.qty = qty;
+		this.price = price;
+		this.totalPrice = totalPrice;
+	}
+
+	// getter methods
+	public String getId() {
+		return id;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	// displayFormat
+	public static void displayFormat() {
+		System.out.format(
+				"-----------------------------------------------------------------------------------------------------------------------------------");
+		System.out.print("\nProduct ID \t\tName\t\tQuantity\t\tRate \t\t\t\tTotal Price\n");
+		System.out.format(
+				"-----------------------------------------------------------------------------------------------------------------------------------\n");
+	}
+
+	// display
+	public void display() {
+		System.out.format("   %-9s             %-9s      %5d               %9.2f                       %14.2f\n", id,
+				pname, qty, price, totalPrice);
+	}
+
+}
